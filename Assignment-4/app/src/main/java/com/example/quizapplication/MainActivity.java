fe749158758bffc4295d941e93f59831f5236016
Kevin <Kevinchau328@gmail.com>
2023-11-21 22:00:03 -0500

    Update Assignment 4

--- Assignment-4/app/src/main/java/com/example/quizapplication/MainActivity.java
+++ Assignment-4/app/src/main/java/com/example/quizapplication/MainActivity.java
@@ -1,27 +1,26 @@
 package com.example.quizapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
-import android.widget.Button;
-import android.widget.TextView;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView totalQuestionsDisplay;
-    TextView questionTextDisplay;
-    Button answerAButton, answerBButton, answerCButton, answerDButton;
-    Button submitButton;
+    private TextView totalQuestionsDisplay;
+    private TextView questionTextDisplay;
+    private Button[] answerButtons;
+    private Button submitButton;
 
-    int score = 0;
-    int totalQuestions = Solutions.question.length;
-    int currentQuestionIndex = 0;
-    String selectedAnswer = "";
-    int solved = 0;
+    private int score = 0;
+    private int totalQuestions;
+    private int currentQuestionIndex = 0;
+    private String selectedAnswer = "";
+    private int solved = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,86 +29,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         totalQuestionsDisplay = findViewById(R.id.total_question);
         questionTextDisplay = findViewById(R.id.question);
-        answerAButton = findViewById(R.id.Answer_A);
-        answerBButton = findViewById(R.id.Answer_B);
-        answerCButton = findViewById(R.id.Answer_C);
-        answerDButton = findViewById(R.id.Answer_D);
+
+        answerButtons = new Button[]{
+                findViewById(R.id.Answer_A),
+                findViewById(R.id.Answer_B),
+                findViewById(R.id.Answer_C),
+                findViewById(R.id.Answer_D)
+        };
+
         submitButton = findViewById(R.id.Submit);
 
-        answerAButton.setOnClickListener(this);
-        answerBButton.setOnClickListener(this);
-        answerCButton.setOnClickListener(this);
-        answerDButton.setOnClickListener(this);
+        for (Button button : answerButtons) {
+            button.setOnClickListener(this);
+        }
+
         submitButton.setOnClickListener(this);
 
-        totalQuestionsDisplay.setText("Questions Left: " + (Integer.parseInt(String.valueOf(totalQuestions)) - solved));
+        totalQuestions = Solutions.question.size();
+        totalQuestionsDisplay.setText("Questions Left: " + (totalQuestions - solved));
 
         loadNewQuestion();
     }
 
     @Override
     public void onClick(View view) {
-        answerAButton.setBackgroundColor(Color.parseColor("#ffffff"));
-        answerBButton.setBackgroundColor(Color.parseColor("#ffffff"));
-        answerCButton.setBackgroundColor(Color.parseColor("#ffffff"));
-        answerDButton.setBackgroundColor(Color.parseColor("#ffffff"));
-
+        resetButtonColors();
         Button clickedButton = (Button) view;
+
         if (clickedButton.getId() == R.id.Submit) {
-            if (selectedAnswer.equals(Solutions.correct[currentQuestionIndex])) {
-                score++;
-                final AlertDialog dialog = new AlertDialog.Builder(this)
-                        .setMessage("Your Answer was Correct.\nThe correct answer was: " + Solutions.correct[currentQuestionIndex])
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss(); // Dismiss the dialog when the user presses OK
-                                currentQuestionIndex++;
-                                loadNewQuestion();
-                                updateSolved();
-                            }
-                        })
-                        .create();
-                dialog.show();
-            } else {
-                final AlertDialog dialog = new AlertDialog.Builder(this)
-                        .setMessage("Your Answer was Incorrect.\nThe correct answer was: " + Solutions.correct[currentQuestionIndex])
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss(); // Dismiss the dialog when the user presses OK
-                                currentQuestionIndex++;
-                                loadNewQuestion();
-                                updateSolved();
-                            }
-                        })
-                        .create();
-                dialog.show();
-            }
+            handleSubmission();
         } else {
             selectedAnswer = clickedButton.getText().toString();
             clickedButton.setBackgroundColor(Color.parseColor("#38b3d9"));
         }
     }
 
-    void loadNewQuestion() {
+    private void resetButtonColors() {
+        for (Button button : answerButtons) {
+            button.setBackgroundColor(Color.parseColor("#ffffff"));
+        }
+    }
+
+    private void handleSubmission() {
+        if (selectedAnswer.equals(Solutions.correct.get(currentQuestionIndex))) {
+            score++;
+        }
+
+        showResultDialog("Your Answer was " +
+                (selectedAnswer.equals(Solutions.correct.get(currentQuestionIndex)) ? "Correct." : "Incorrect.") +
+                "\nThe correct answer was: " + Solutions.correct.get(currentQuestionIndex));
+
+        currentQuestionIndex++;
+        updateSolved();
+    }
+
+    private void showResultDialog(String message) {
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        loadNewQuestion();
+                    }
+                })
+                .create();
+        dialog.show();
+    }
+
+    private void loadNewQuestion() {
         if (currentQuestionIndex == totalQuestions) {
             finishQuiz();
             return;
         }
-        questionTextDisplay.setText(Solutions.question[currentQuestionIndex]);
-        answerAButton.setText(Solutions.options[currentQuestionIndex][0]);
-        answerBButton.setText(Solutions.options[currentQuestionIndex][1]);
-        answerCButton.setText(Solutions.options[currentQuestionIndex][2]);
-        answerDButton.setText(Solutions.options[currentQuestionIndex][3]);
+
+        questionTextDisplay.setText(Solutions.question.get(currentQuestionIndex));
+
+        for (int i = 0; i < answerButtons.length; i++) {
+            answerButtons[i].setText(Solutions.options.get(currentQuestionIndex).get(i));
+        }
     }
-    void updateSolved(){
+
+    private void updateSolved() {
         solved++;
-        totalQuestionsDisplay.setText("Questions Left: " + (Integer.parseInt(String.valueOf(totalQuestions)) - solved));
+        totalQuestionsDisplay.setText("Questions Left: " + (totalQuestions - solved));
     }
-    void finishQuiz() {
+
+    private void finishQuiz() {
         new AlertDialog.Builder(this)
-                .setMessage("Your Score is: " + score + "\n" + "Out of: " + totalQuestions + " questions")
+                .setMessage("Your Score is: " + score + "/" + totalQuestions)
                 .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -120,12 +128,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .show();
     }
 
-    void restartQuiz() {
-        //Initialize everything to default
+    private void restartQuiz() {
         score = 0;
         currentQuestionIndex = 0;
         selectedAnswer = "";
-        totalQuestionsDisplay.setText("Questions left: " + (totalQuestions));
+        solved = 0;
+        totalQuestionsDisplay.setText("Questions left: " + totalQuestions);
         loadNewQuestion();
     }
 }
--- Assignment-4/app/src/main/java/com/example/quizapplication/Solutions.java
+++ Assignment-4/app/src/main/java/com/example/quizapplication/Solutions.java
@@ -1,27 +1,31 @@
 package com.example.quizapplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solutions {
-    public static String question[] = {
+    public static List<String> question = Arrays.asList(
             "What is the fastest land animal?",
             "What was the most used programming language in 2023?",
             "What year did the dot com bubble begin to burst?",
             "Who was the first televised president?",
-            "Which character is not from the simpsons?"
-    };
+            "Which character is not from the simpsons"
+    );
 
-    public static String options[][] = {
-            {"Cheetah", "Cougar", "Ostrich", "Horse"},
-            {"Python", "JavaScript", "C++", "HTML"},
-            {"1999", "2003", "2005", "2000"},
-            {"Franklin Roosevelt", "Donald Trump", "Abraham Lincoln", "Joe Biden"},
-            {"Homer", "Vince", "Lisa", "Kay"}
-    };
+    public static List<List<String>> options = Arrays.asList(
+            Arrays.asList("Cheetah", "Cougar", "Ostrich", "Horse"),
+            Arrays.asList("Python", "JavaScript", "C++", "HTML"),
+            Arrays.asList("1999", "2003", "2005", "2000"),
+            Arrays.asList("Franklin Roosevelt", "Donald Trump", "Abraham Lincoln", "Joe Biden"),
+            Arrays.asList("Homer", "Vince", "Lisa", "Kay")
+    );
 
-    public static String correct[] = {
+    public static List<String> correct = Arrays.asList(
             "Cheetah",
             "Python",
             "2000",
             "Franklin Roosevelt",
-            "Kay",
-    };
+            "Kay"
+    );
 }
